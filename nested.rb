e60397b6cb3f8fb25569afb740f9d03a5fe9fc80
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-13 18:55:41 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -68,10 +68,6 @@ def adding_matz
 # }
 
 
-programmer_hash[:yukihiro_matsumoto => {
-   :known_for => "Ruby",
-    :languages => ["LISP", "C"]
- }]
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -87,7 +83,11 @@ programmer_hash[:yukihiro_matsumoto => {
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:yukihiro_matsumoto => {
+		    :known_for => "Ruby",
+		     :languages => ["LISP", "C"]
+		  }]
+			programmer_hash
 
 end
 
